6304bd63edd47a5b58a7f0208f8966df78627901
Eyas Anwar <eyasanwar@gmail.com>
2023-06-01 13:06:26 +0300

    refactor flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -20,50 +20,67 @@ telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url
 
 secret = configs['telegram-bot']['secret']
 bot = telepot.Bot(configs['telegram-bot']['token'])
-print(configs['telegram-bot']['webhook'])
-print(configs['telegram-bot']['webhook'].format(secret))
 bot.setWebhook(configs['telegram-bot']['webhook'].format(secret), max_connections=1)
 marks_finder = MarksFinder(configs['marks']['marks-file'])
 template_filler = TemplateFiller(configs['marks']['template-message'])
 
 app = Flask(__name__)
 
+def validate_request(request):
+    json_req = request.get_json()
+    if "message" not in json_req:
+        return "Error", None
+    if "text" not in json_req["message"]:
+        return "Error", json_req["message"]["chat"]["id"]
+    return None, json_req["message"]["chat"]["id"]
+    
+def validate_student_id(text):
+    if not text.isdigit():
+        return "Error", messages['enter-student-id']
+    if len(text) != 10:
+        return "Error", messages['wrong-student-id']
+    return None, ""
+
+def start_command(chat_id):
+    bot.sendMessage(chat_id, messages['welcome-message'])
+    return "OK"
+
+def student_id_command(json_req):
+    chat_id = json_req["message"]["chat"]["id"]
+    std_id = json_req["message"]["text"]
+    err, message = validate_student_id(std_id)
+    if err:
+        bot.sendMessage(chat_id, message)
+        return "OK"
+    std_id = unidecode(std_id)
+    db = Database(configs['database'])
+    is_found, student = marks_finder.find(std_id.strip())
+    if not is_found:
+        bot.sendMessage(chat_id, messages['ensure-student-id'])
+        return "OK"
+    is_registered, reg_id = db.check_id(json_req["message"]["from"]["id"])
+    if is_registered and reg_id != std_id.strip():
+        bot.sendMessage(chat_id, messages['query-your-own-id'])
+        return "OK"
+    if not is_registered:
+        db.insert_student(std_id.strip(), json_req["message"]["from"]["id"])
+    json_req['student_name'] = student['name']
+    resp = template_filler.fill(student)
+    db.insert_log(json.dumps(json_req))
+    bot.sendMessage(chat_id, resp)
 
 @app.route('/{}'.format(secret), methods=["POST"])
 def telegram_webhook():
     try:
-        update = request.get_json()
-        if "message" in update:
-            chat_id = update["message"]["chat"]["id"]
-            if "text" in update["message"]:
-                text = update["message"]["text"]
-                if text == '/start':
-                    bot.sendMessage(chat_id, messages['welcome-message'])
-                else:
-                    if not text.isdigit():
-                        bot.sendMessage(chat_id, messages['enter-student-id'])
-                        return "OK"
-                    if len(text) != 10:
-                        bot.sendMessage(chat_id, messages['wrong-student-id'])
-                        return "OK"
-                    text = unidecode(text)
-                    db = Database(configs['database'])
-                    is_found, student = marks_finder.find(text.strip())
-                    if not is_found:
-                        bot.sendMessage(chat_id, messages['ensure-student-id'])
-                        return "OK"
-                    is_registered, reg_id = db.check_id(update["message"]["from"]["id"])
-                    if is_registered and reg_id != text.strip():
-                        bot.sendMessage(chat_id, messages['query-your-own-id'])
-                        return "OK"
-                    if not is_registered:
-                        db.insert_student(text.strip(), update["message"]["from"]["id"])
-                    update['student_name'] = student['name']
-                    resp = template_filler.fill(student)
-                    db.insert_log(json.dumps(update))
-                    bot.sendMessage(chat_id, resp)
-            else:
+        err, chat_id = validate_request(request)
+        if err:
+            if chat_id:
                 bot.sendMessage(chat_id, messages['unknown-message'])
+            return "OK"
+        update = request.get_json()
+        text = update["message"]["text"]
+        if text == '/start': return start_command(chat_id)
+        return student_id_command(update)
     except Exception as ex:
         pass
     return "OK"
